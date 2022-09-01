3be0e517fef7dfad1913ea6962321b4a23bd66ab
Sage Skaff <skaffsage@gmail.com>
2022-08-31 20:21:16 -0700

    Test: item update endpoint

--- spec/requests/api/v1/item_requests_spec.rb
+++ spec/requests/api/v1/item_requests_spec.rb
@@ -67,4 +67,18 @@ RSpec.describe 'Items API' do
     expect(created_item.unit_price).to eq(item_params[:unit_price])
     expect(created_item.merchant_id).to eq(item_params[:merchant_id])
   end
+
+  it 'can update an existing item' do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: 'Worm on a String' }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({ item: item_params })
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq('Worm on a String')
+  end
 end
