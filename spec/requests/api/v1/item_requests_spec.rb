83c82a18cd771687d8a8af1584f8e01d0a0d1515
Sage Skaff <skaffsage@gmail.com>
2022-08-31 20:12:57 -0700

    Test: item create endpoint

--- spec/requests/api/v1/item_requests_spec.rb
+++ spec/requests/api/v1/item_requests_spec.rb
@@ -42,5 +42,29 @@ RSpec.describe 'Items API' do
 
     expect(item).to have_key(:description)
     expect(item[:description]).to be_an(String)
+
+    expect(item).to have_key(:unit_price)
+    expect(item[:unit_price]).to be_an(Float)
+  end
+
+  it 'can create a new item' do
+    id = create(:merchant).id
+
+    item_params = {
+      name: 'Coffee Mug',
+      description: 'Holds coffee, tea, or other warm beverages.',
+      unit_price: 10.50,
+      merchant_id: id
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+
+    post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
   end
 end
