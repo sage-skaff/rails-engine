c375dafcd1bbd2aa7234765f63da48df8bf571e4
Sage Skaff <skaffsage@gmail.com>
2022-08-31 19:54:37 -0700

    Test: items index and show endpoints

---
+++ spec/requests/api/v1/item_requests_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe 'Items API' do
+  it 'sends a list of items' do
+    create_list(:item, 5)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_an(String)
+
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_an(String)
+
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_an(Float)
+    end
+  end
+
+  it 'can get one item by its id' do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(Integer)
+
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_an(String)
+
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_an(String)
+  end
+end
