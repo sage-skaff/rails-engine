8c206405813169afd6d84206dcffa79376fbd809
Sage Skaff <skaffsage@gmail.com>
2022-08-31 19:43:47 -0700

    Test: merchants show endpoint

--- spec/requests/api/v1/merchant_requests_spec.rb
+++ spec/requests/api/v1/merchant_requests_spec.rb
@@ -6,6 +6,32 @@ RSpec.describe 'Merchants API' do
 
     get '/api/v1/merchants'
 
-    expect(response).to be_sucessful
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_an(String)
+    end
+  end
+
+  it 'can get one merchant by their id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(Integer)
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_an(String)
   end
 end
