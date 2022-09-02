5d92c27461d57ccf9689d231362896ceecca064b
Sage Skaff <skaffsage@gmail.com>
2022-09-02 08:57:33 -0700

    Update

--- spec/requests/api/v1/merchant_requests_spec.rb
+++ spec/requests/api/v1/merchant_requests_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe 'Merchants API' do
 
     merchants = JSON.parse(response.body, symbolize_names: true)
 
-    merchants.each do |merchant|
+    merchants[:data].each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_an(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_an(String)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_an(String)
     end
   end
 
@@ -26,12 +26,12 @@ RSpec.describe 'Merchants API' do
 
     expect(response).to be_successful
 
-    merchant = JSON.parse(response.body, symbolize_names: true)
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(merchant).to have_key(:id)
-    expect(merchant[:id]).to be_an(Integer)
+    expect(merchant[:id]).to be_an(String)
 
-    expect(merchant).to have_key(:name)
-    expect(merchant[:name]).to be_an(String)
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_an(String)
   end
 end
